048a5487a6282cd598488edac871d5884339cfa0
Rescue9 <andrew.buskov@gmail.com>
2025-05-18 12:35:40 -0500

    implement 24 hour time

---
+++ app/src/main/java/com/corridor9design/simplyweather/utils/Units.java
@@ -0,0 +1,11 @@
+package com.corridor9design.simplyweather.utils;
+
+public class Units {
+    public final String temperature;
+    public final String speed;
+
+    public Units(String temperature, String speed) {
+        this.temperature = temperature;
+        this.speed = speed;
+    }
+}
