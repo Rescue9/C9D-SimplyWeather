18c3b6d00ee6aa47b4a350cef50779a1251daa28
Rescue9 <andrew.buskov@gmail.com>
2025-05-18 17:38:07 +0000

    reformat code

--- app/src/main/java/com/corridor9design/simplyweather/utils/UnitsHelper.java
+++ app/src/main/java/com/corridor9design/simplyweather/utils/UnitsHelper.java
@@ -3,22 +3,23 @@ package com.corridor9design.simplyweather.utils;
 import com.corridor9design.simplyweather.preferences.AppPreferences;
 
 public class UnitsHelper {
+  public static String getTemperatureUnit(AppPreferences prefs) {
+    return prefs.getBooleanPreference("imperial_units", false) ? "°F" : "°C";
+  }
 
-    public static String getTemperatureUnit(AppPreferences prefs) {
-        return prefs.getBooleanPreference("imperial_units", false) ? "°F" : "°C";
-    }
+  public static String getSpeedUnit(AppPreferences prefs) {
+    return prefs.getBooleanPreference("imperial_units", false) ? "mph" : "kph";
+  }
 
-    public static String getSpeedUnit(AppPreferences prefs) {
-        return prefs.getBooleanPreference("imperial_units", false) ? "mph" : "kph";
-    }
+  public static String is24HourTimeFormat(AppPreferences prefs) {
+    return prefs.getBooleanPreference("use_24_hour", false)
+        ? "EEEE HH:mm:ss"
+        : "EEEE hh:mm a";
+  }
 
-    public static String is24HourTimeFormat(AppPreferences prefs) {
-        return prefs.getBooleanPreference("use_24_hour", false) ? "EEEE HH:mm:ss" : "EEEE hh:mm a";
-    }
-
-    public static Units getUnits(AppPreferences prefs) {
-        return prefs.getBooleanPreference("imperial_units", false)
-            ? new Units("°F", "mph")
-            : new Units("°C", "kph");
-    }
+  public static Units getUnits(AppPreferences prefs) {
+    return prefs.getBooleanPreference("imperial_units", false)
+        ? new Units("°F", "mph")
+        : new Units("°C", "kph");
+  }
 }
